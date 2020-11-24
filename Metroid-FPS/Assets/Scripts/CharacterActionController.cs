599373708108540050cbdfa92b3b7b99d0d0674f
Humblebee89 <joemaleski08@gmail.com>
2020-11-23 21:02:26 -0500

    General bullshit, I don't even remember

---
+++ Metroid-FPS/Assets/Scripts/CharacterActionController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterActionController : MonoBehaviour
+{
+    [SerializeField] private Transform playerBody;
+    [SerializeField] private Transform playerCamera;
+
+    private PlayerInput playerInput;
+
+    private void Start()
+    {
+        playerInput = new PlayerInput();
+    }
+
+    private void Update()
+    {
+
+    }
+}
