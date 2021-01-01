07e356a140c2b2a732c74ff60fe742ca20a3bf31
Humblebee89 <joemaleski08@gmail.com>
2021-01-01 00:56:45 -0500

    Particle Effects

--- Metroid-FPS/Assets/Scripts/Shared/DestroyParticleSystemAfterDuration.cs
+++ Metroid-FPS/Assets/Scripts/Shared/DestroyParticleSystemAfterDuration.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class DestroyParticleSystemAfterDuration : MonoBehaviour
 {
-    private ParticleSystem myParticleSystem;
+    [SerializeField] private ParticleSystem myParticleSystem;
 
     private void Awake()
     {
-        myParticleSystem = GetComponent<ParticleSystem>();
-
         Destroy(gameObject, myParticleSystem.main.duration);
     }
 }
