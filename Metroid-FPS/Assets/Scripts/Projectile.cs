3752163014ac46e68ecf79d3fefeefbaeaa46938
Humblebee89 <joemaleski08@gmail.com>
2020-12-31 20:43:27 -0500

    Particle trail on projectile

--- Metroid-FPS/Assets/Scripts/Projectile.cs
+++ Metroid-FPS/Assets/Scripts/Projectile.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float projectileSpeed = 1.0f;
+    [SerializeField] private float destroyTimer = 4.0f;
 
     private Rigidbody projectileRigidbody;
 
     private void Awake()
     {
         projectileRigidbody = GetComponent<Rigidbody>();
+        Destroy(gameObject, destroyTimer);
     }
 
     private void FixedUpdate()
