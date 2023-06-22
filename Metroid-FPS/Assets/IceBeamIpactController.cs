1d8bf6063da6c22eb5de81d8f7e460648200f44c
Humblebee89 <joemaleski08@gmail.com>
2023-06-21 20:29:03 -0700

    Added Ice beam impact effect

---
+++ Metroid-FPS/Assets/IceBeamIpactController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBeamIpactController : MonoBehaviour
+{
+    public void DestroyEffect()
+    {
+        Destroy(gameObject);
+    }
+}
