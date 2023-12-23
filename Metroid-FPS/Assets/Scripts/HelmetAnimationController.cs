89cc97a98969a6479bfcba0da43dbb16013ec00e
Humblebee89 <joemaleski08@gmail.com>
2023-12-23 09:06:43 -0800

    Fixed rotation bug with helmet

--- Metroid-FPS/Assets/Scripts/HelmetAnimationController.cs
+++ Metroid-FPS/Assets/Scripts/HelmetAnimationController.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class HelmetAnimationController : MonoBehaviour
 {
     [SerializeField] private CameraLookController cameraLookController;
+    [SerializeField] private float magnitude = 0.5f;
+    [SerializeField] private float speed = 0.1f;
 
     private Vector2 inputDirection;
+    private Quaternion targetRotation;
+    private float timeCount = 0f;
 
     private void Update()
     {
-        inputDirection = new Vector2(-cameraLookController.inputDirection.x, cameraLookController.inputDirection.y * 0.5f);
-        transform.localRotation = Quaternion.Euler(inputDirection.y, inputDirection.x, transform.rotation.z);
+        inputDirection = new Vector2(-cameraLookController.inputDirection.x, cameraLookController.inputDirection.y * magnitude);
+        targetRotation = Quaternion.Euler(inputDirection.y, inputDirection.x, transform.rotation.z);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime * speed);
     }
 }
