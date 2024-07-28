39a807b02bc2ba3a488f5dcc2cf837eba8be0c9b
Humblebee89 <joemaleski08@gmail.com>
2024-07-28 09:47:42 -0700

    Added Red hit effect to Metroid Plushy

--- Metroid-FPS/Assets/Scripts/Damageable.cs
+++ Metroid-FPS/Assets/Scripts/Damageable.cs
@@ -9,24 +9,46 @@ public class Damageable : MonoBehaviour
     [SerializeField] private bool destroyObjectOnDeath;
     [SerializeField] private GameObject objectToDestroy;
     [SerializeField] private GameObject deathParticleEffect;
+    [SerializeField] private float hitEffectColorDecayRate = 0.01f;
 
     private int health;
     private bool dead;
+    private Material material;
+    private float redBlendAmount;
 
     public UnityEvent onDeath;
 
     private void Start()
     {
         health = maxHealth;
+        material = this.GetComponent<MeshRenderer>().material;
+    }
+
+    private void Update()
+    {
+        UpdateMaterialColor();
     }
 
     public void TakeDamage(int amount)
     {
-        if(health > 0)
+        if (health > 0)
+        {
             health -= amount;
+            redBlendAmount = 1f;
+        }
 
         if (health <= 0)
             KillDamageable();
+    }
+
+
+    private void UpdateMaterialColor()
+    {
+        if (redBlendAmount <= 0f)
+            return;
+
+        redBlendAmount -= hitEffectColorDecayRate;
+        material.SetFloat("_HitColorBlendAmount", redBlendAmount);
     }
 
     private void KillDamageable()
